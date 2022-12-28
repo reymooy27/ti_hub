4d0b60c13d460dae5262a2806b51fa1bf330c6a7
Rey Mooy <gdrrey@gmail.com>
2022-12-28 23:19:45 +0800

    delete protected route example

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -3,7 +3,6 @@ import { createRouter } from "./context";
 import superjson from "superjson";
 
 import { postRouter } from "./post";
-import { protectedExampleRouter } from "./protected-example-router";
 import { commentRouter } from "./comment";
 import { userRouter } from "./user";
 
@@ -11,7 +10,6 @@ export const appRouter = createRouter()
   .transformer(superjson)
   .merge("post.", postRouter)
   .merge("comment.", commentRouter)
-  .merge("auth.", protectedExampleRouter)
   .merge("user.", userRouter);
 
 // export type definition of API
