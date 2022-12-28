cd5560c159b5e970725fa60cd627bdb5c4ea08f3
Rey Mooy <gdrrey@gmail.com>
2022-12-28 22:58:07 +0800

    merge user route to index route

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -5,12 +5,14 @@ import superjson from "superjson";
 import { postRouter } from "./post";
 import { protectedExampleRouter } from "./protected-example-router";
 import { commentRouter } from "./comment";
+import { userRouter } from "./user";
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge("post.", postRouter)
   .merge("comment.", commentRouter)
-  .merge("auth.", protectedExampleRouter);
+  .merge("auth.", protectedExampleRouter)
+  .merge("user.", userRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
