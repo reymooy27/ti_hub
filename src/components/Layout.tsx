a98da433536aa5a232d2f79927df5e6f750c547a
Rey Mooy <gdrrey@gmail.com>
2023-01-04 22:00:42 +0800

    add sm breakpoint

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({children}: {children: ReactElement}) {
       <Header/>
       <main className='w-full h-full pt-[70px] pb-5 flex gap-6 px-6 relative'>
         <LeftSideBar/> 
-          <div className='w-[66%] h-full flex flex-col justify-start gap-3'>
+          <div className="sm:w-[66%] w-full h-full flex flex-col justify-start gap-3">
             {children}
           </div>
         <RightSideBar/>
