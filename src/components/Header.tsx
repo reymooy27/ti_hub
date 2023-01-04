ddac4408f388353e95aa2e5cebbefa92bfe3012f
Rey Mooy <gdrrey@gmail.com>
2023-01-04 21:59:36 +0800

    add home icon & delete the menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import { signOut, useSession } from 'next-auth/react'
 import Link from 'next/link';
 import React from 'react'
 import dynamic from 'next/dynamic'
+const FontAwesomeIcon = dynamic(()=> import('@fortawesome/react-fontawesome').then(module=> module.FontAwesomeIcon))
 const DropdownMenu = dynamic(()=> import('./DropdownMenu'))
 
 export default function Header() {
@@ -9,31 +10,27 @@ export default function Header() {
   const {data: session} = useSession();
 
   return (
-    <header className="flex justify-between w-full bg-secondary h-[60px] px-6 gap-6 fixed z-[3]">
-      <div className='w-[33%] flex justify-start items-center gap-6'>
-        <div className='bg-white rounder w-[40px] h-[40px]'></div>
+    <header className="flex justify-between w-full bg-secondary h-[60px] px-6 fixed z-[3]">
+      <div className='w-full flex justify-start items-center gap-5'>
+        <Link href='/'>
+          <a>
+            <FontAwesomeIcon icon='home' className='w-[26px] h-[26px]'/> 
+          </a>
+        </Link>
         <div className='w-full'>
-          <input type="text" className='w-full h-[40px] p-4 border bg-secondary text-white border-primary rounded-[15px]' />
+          <input type="text" className='w-full max-w-[300px] h-[40px] p-4 border bg-secondary text-white border-primary rounded-[15px]' />
         </div>
       </div>
-      <div className='w-[66%] flex justify-center items-center gap-6'>
-        <Link href='/'>
-          <a className='font-bold'>Home</a>
-        </Link>
-        <Link href='/chats'>
-          <a className='font-bold'>Chats</a>
-        </Link>
-        <Link href={`/user/${session?.user?.id}`}>
-          <a className='font-bold'>Profile</a>
-        </Link>
-        <Link href='/settings'>
-          <a className='font-bold'>Setting</a>
-        </Link>
-        
-      </div>
-      <div className='w-[33%] flex justify-end items-center'>
+      <div className='w-full flex justify-end items-center'>
         {session ? 
-          <DropdownMenu session={session} signOut={signOut}/>
+          <div className='flex justify-center items-center gap-5'>
+            <Link href='/chats'>
+              <a>
+                <FontAwesomeIcon icon='message' className='w-[26px] h-[26px]'/> 
+              </a>
+            </Link>
+            <DropdownMenu session={session} signOut={signOut}/>
+          </div>
         :
           <Link className='bg-red-500' href="/login">
             <a>Login</a>
