5fbed9f92795b64a8db149f933ee70c98ab511fa
Rey Mooy <gdrrey@gmail.com>
2023-01-11 20:35:51 +0800

    change import

--- src/components/TweetInput.tsx
+++ src/components/TweetInput.tsx
@@ -1,12 +1,10 @@
-import dynamic from 'next/dynamic'
-const CloseButton = dynamic(()=> import('@chakra-ui/react').then(module=> module.CloseButton))
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image'
 import Link from 'next/link'
 import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
 import { useSession } from 'next-auth/react'
 import { getBaseUrl } from '../pages/_app'
-import { Spinner } from '@chakra-ui/react'
+import { CloseButton, Spinner } from '@chakra-ui/react'
 
 export default function TweetInput() {
 
