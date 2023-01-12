426c07ea639592ff9e2ff76cc6fe2cc7f99fceaf
Rey Mooy <gdrrey@gmail.com>
2023-01-12 23:07:55 +0800

    add suspense

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react'
+import dynamic from 'next/dynamic'
+import React, { Suspense, useEffect } from 'react'
 import { trpc } from '../utils/trpc'
-import Post from './Post'
+const Post = dynamic(()=> import('./Post'))
 import PostSkeleteon from './PostSkeleteon'
 import TweetInput from './TweetInput'
 
@@ -24,27 +25,27 @@ export default function Main() {
   return (
     <div className="sm:w-[66%] w-full h-full flex flex-col justify-start gap-3">
       <TweetInput/>
-      {posts.isLoading 
-      ? Array(5).fill('').map((p,i)=> (<PostSkeleteon key={i}/>))   
-      : 
-        posts?.data?.length as number < 1 ? <h1>No Post</h1> : 
-        posts?.data?.map((post)=>(
-          <div onClick={saveScrollPosition} key={post.id}>
-            <Post 
-              key={post.id} 
-              postId={post.id}
-              title={post.title} 
-              username={post.user.name} 
-              profileImage={post.user.image} 
-              image={post.image} 
-              createdAt={post.createdAt}
-              likes={post.likes}
-              count={post._count}
-              userId={post.userId}
-              />
-          </div>
-        ))
-      }
+      <Suspense>
+        {posts.isLoading && Array(5).fill('').map((p,i)=> (<PostSkeleteon key={i}/>))} 
+        {posts?.data?.length as number < 1 && <h1>No Post</h1>}
+        {posts?.data?.map((post)=>(
+            <div onClick={saveScrollPosition} key={post.id}>
+              <Post 
+                key={post.id} 
+                postId={post.id}
+                title={post.title} 
+                username={post.user.name} 
+                profileImage={post.user.image} 
+                image={post.image} 
+                createdAt={post.createdAt}
+                likes={post.likes}
+                count={post._count}
+                userId={post.userId}
+                />
+            </div>
+          ))
+        }
+      </Suspense>
     </div>
   )
 }
